4207146d37b603c841ea7fca9b651509006213c4
Brayan Guardado <guardadobrian55@gmail.com>
2026-02-27 13:12:19 -0600

    Se actualizo la card de los productos

--- src/components/features/products/ProductCard.tsx
+++ src/components/features/products/ProductCard.tsx
@@ -23,7 +23,7 @@ const ProductCard = ({ product }: { product: Product }) => {
     <div
       onClick={handleContainerClick}
       className={`
-    flex items-center justify-between
+    flex items-center justify-between w-full
     px-4 py-3
     rounded-2xl
     border
